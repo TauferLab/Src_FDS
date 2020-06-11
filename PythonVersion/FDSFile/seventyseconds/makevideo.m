f0f34b9642037efe893ec17e0e560380445f1fb0
Leobardo Valera <leobardovalera@gmail.com>
2020-06-11 13:14:07 -0400

    updating version of FDS creation file tool

---
+++ PythonVersion/FDSFile/seventyseconds/makevideo.m
@@ -0,0 +1,13 @@
+video =VideoWriter('fire.avi','Motion JPEG AVI');
+video.FrameRate = 2;
+   
+    
+open(video); %open the file for writing
+    
+    for i=0:140
+        numero = sprintf('%0.4d',i);
+        name = ['Chimney_tops_1m_',numero,'.png']
+        I = imread(name); %read the next image
+        writeVideo(video,I); %write the image to file(name,'-dpng')
+    end
+close(video); %close the file
---
+++ PythonVersion/FDSFile/seventyseconds2m/fuego.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export OMP_NUM_THREADS=4
+mpiexec -n $1 fds FDSPROG.fds
