0b9889756f65fd796ebf9e1e967f1641145a3478
Leobardo Valera <leobardovalera@gmail.com>
2020-07-02 15:15:43 -0600

    first commit from my WSL testing if everything works

--- MatLabVersion/example.m
+++ MatLabVersion/example.m
@@ -1,3 +1,3 @@
-file_path = '/home/leobardo/Documents/Dr Taufer Lab/experiments/Src_FDS/PythonVersion/MoistureFile/MoistureAndElevationUTMTraslated201610.csv';
+file_path = 'D:\documents\Dr Taufer Lab\experiments\Src_FDS\PythonVersion\MoistureFile\MoistureAndElevationUTMTraslated201610.csv';
 
 plot_elevation(file_path)
