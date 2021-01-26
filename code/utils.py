526a49a5a76909b4d64249463e0965db9be1e836
Leobardo Valera <leobardovalera@gmail.com>
2021-01-26 12:01:44 -0500

    Eliminating the zero cells in hrr

--- code/utils.py
+++ code/utils.py
@@ -220,9 +220,9 @@ def restart_fds_file(T_begin, T_end, DT, PC, Nmx, Nmy, Nmz, Hrr, Child):
         elevation = return_elevation(Mst, x, y)
         diferencia = elevation - Hrr['z'][ind]
         if (diferencia>0):
-            fds.write(f"&INIT XB={Hrr['x'][ind]},{Hrr['x'][ind]+Ro},{Hrr['y'][ind]},{Hrr['y'][ind]+Ro},{math.ceil(Hrr['z'][ind]+diferencia)},{math.ceil(Hrr['z'][ind])+Ro+diferencia}, HRRPUV={math.ceil(Hrr['hrr'][ind])}, RAMP_Q='fire' /\n")
+            fds.write(f"&INIT XB={Hrr['x'][ind]},{Hrr['x'][ind]+Ro},{Hrr['y'][ind]},{Hrr['y'][ind]+Ro},{math.ceil(Hrr['z'][ind])},{math.ceil(Hrr['z'][ind])+Ro}, HRRPUV={math.ceil(Hrr['hrr'][ind])}, RAMP_Q='fire' /\n")
         else:
-            fds.write(f"&INIT XB={Hrr['x'][ind]},{Hrr['x'][ind]+Ro},{Hrr['y'][ind]},{Hrr['y'][ind]+Ro},{math.ceil(Hrr['z'][ind]-diferencia)},{math.ceil(Hrr['z'][ind])+Ro-diferencia}, HRRPUV={math.ceil(Hrr['hrr'][ind])}, RAMP_Q='fire' /\n")         
+            fds.write(f"&INIT XB={Hrr['x'][ind]},{Hrr['x'][ind]+Ro},{Hrr['y'][ind]},{Hrr['y'][ind]+Ro},{math.ceil(Hrr['z'][ind])},{math.ceil(Hrr['z'][ind])+Ro}, HRRPUV={math.ceil(Hrr['hrr'][ind])}, RAMP_Q='fire' /\n")         
     
     fds.write(f"\n")
     
