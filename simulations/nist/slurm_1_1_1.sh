6e4bd16ba32f9b3bb4d6672f2f78eac8aab09b35
Leobardo Valera <lvalera@login1.frontera.tacc.utexas.edu>
2021-09-10 12:05:00 -0500

    Updating the scripts and the input files of FDS

--- simulations/nist/slurm_1_1_1.sh
+++ simulations/nist/slurm_1_1_1.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name=FDS
 #SBATCH --output=%x-%j.out                          # %x = Job Name & %j = Job Id
 #SBATCH --error=%x-%j.err                          
-#SBATCH --nodes=1                                   # Total number of nodes
-#SBATCH --ntasks=8                                  # Total number of MPI tasks
-#SBATCH --time=2:00:00                              # Run time (d-hh:mm:ss)
-#SBATCH --partition=development                            # Replace queue_name with appropriate partition name (ex. skx-normal)
+#SBATCH --nodes=2                                   # Total number of nodes
+#SBATCH --ntasks=64                                  # Total number of MPI tasks
+#SBATCH --time=12:00:00                              # Run time (d-hh:mm:ss)
+#SBATCH --partition=small                           # Replace queue_name with appropriate partition name (ex. skx-normal)
 #SBATCH --mail-user=leobardovalera@gmail.com        # Replace email with your preferred email
 #SBATCH --mail-type=all                             # Send email at start & end of job
 #SBATCH -A ASC21014                                 # Allocation name (req'd if you have more than 1
