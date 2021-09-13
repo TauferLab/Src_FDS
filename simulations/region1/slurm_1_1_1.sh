e1b1301faa80499fd88c73ed99075b410d7da592
Leobardo Valera <leobardovalera@gmail.com>
2021-09-13 09:59:37 -0600

    Adding the original and texted images of Gatlinburg

--- simulations/region1/slurm_1_1_1.sh
+++ simulations/region1/slurm_1_1_1.sh
@@ -4,9 +4,9 @@
 #SBATCH --output=%x-%j.out                          # %x = Job Name & %j = Job Id
 #SBATCH --error=%x-%j.err                          
 #SBATCH --nodes=1                                   # Total number of nodes
-#SBATCH --ntasks=8                                  # Total number of MPI tasks
+#SBATCH --ntasks=16                                  # Total number of MPI tasks
 #SBATCH --time=2:00:00                              # Run time (d-hh:mm:ss)
-#SBATCH --partition=small                            # Replace queue_name with appropriate partition name (ex. skx-normal)
+#SBATCH --partition=development                            # Replace queue_name with appropriate partition name (ex. skx-normal)
 #SBATCH --mail-user=leobardovalera@gmail.com        # Replace email with your preferred email
 #SBATCH --mail-type=all                             # Send email at start & end of job
 #SBATCH -A ASC21014                                 # Allocation name (req'd if you have more than 1
